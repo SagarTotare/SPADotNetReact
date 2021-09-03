58a5f47587b57fd39231c6f6a02a4d3e37d15353
Sagar Totare <STotare@dealersocket.com>
2021-09-03 13:09:44 +0530

    Added react app

---
+++ react-app/src/components/Login/login.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
+import axios from 'axios';
+
+import logo from "../../logo.svg";
+
+const LoginForm = () => {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const postData = () => {
+        axios.post(`http://localhost:54306/api/user/Login`, { userName, password }).then(response => {
+            alert(response.data);
+        });
+    }
+
+    return (
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <Header as='h2' color='teal' textAlign='center'>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    Log-in to your account
+                </Header>
+                <Form size='large'>
+                    <Segment stacked>
+                        <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' onChange={ e => setUserName(e.target.value)} />
+                        <Form.Input
+                            fluid
+                            icon='lock'
+                            iconPosition='left'
+                            placeholder='Password'
+                            type='password'
+                            onChange={ e => setPassword(e.target.value)}
+                        />
+                        <Button color='teal' fluid size='large'  onClick={postData}>
+                            Login
+                        </Button>
+                    </Segment>
+                </Form>
+                <Message>
+                    New to us? <a href='./register'>Sign Up</a>
+                </Message>
+            </Grid.Column>
+        </Grid>
+    )
+};
+
+export default LoginForm
